ffd20d8ebdafb90a2792e2e75e3c458697b58b45
DESKTOP-9DHKK95\leons <leonsys7@gmail.com>
2021-09-16 10:39:47 +0200

    Refresh Token is added and working.

--- WorkNet/src/main/java/com/example/worknet/controllers/AuthController.java
+++ WorkNet/src/main/java/com/example/worknet/controllers/AuthController.java
@@ -7,16 +7,21 @@ import java.util.stream.Collectors;
 
 //import javax.validation.Valid;
 
+import com.example.worknet.exception.TokenRefreshException;
 import com.example.worknet.model.ERole;
+import com.example.worknet.model.RefreshToken;
 import com.example.worknet.model.Role;
 import com.example.worknet.model.User;
 import com.example.worknet.payload.request.LoginRequest;
 import com.example.worknet.payload.request.SignupRequest;
+import com.example.worknet.payload.request.TokenRefreshRequest;
 import com.example.worknet.payload.response.JwtResponse;
 import com.example.worknet.payload.response.MessageResponse;
+import com.example.worknet.payload.response.TokenRefreshResponse;
 import com.example.worknet.repository.RoleRepository;
 import com.example.worknet.repository.UserRepository;
 import com.example.worknet.security.jwt.JwtUtils;
+import com.example.worknet.security.services.RefreshTokenService;
 import com.example.worknet.security.services.UserDetailsImpl;
 
 
@@ -41,6 +46,8 @@ import javax.validation.Valid;
 public class AuthController {
 
 
+    @Autowired
+    RefreshTokenService refreshTokenService;
 
     private AuthenticationManager authenticationManager;
 
@@ -52,7 +59,7 @@ public class AuthController {
 
     private PasswordEncoder encoder;
 
-
+    @Autowired
     private JwtUtils jwtUtils;
 
     @Autowired
@@ -65,25 +72,41 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser( @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+
+        String jwt = jwtUtils.generateJwtToken(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+        return ResponseEntity.ok(new JwtResponse(jwt, refreshToken.getToken(), userDetails.getId(),
+                userDetails.getUsername(), userDetails.getEmail(), roles));
     }
+
+    @PostMapping("/refreshtoken")
+    public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
+        String requestRefreshToken = request.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String token = jwtUtils.generateTokenFromUsername(user.getUsername());
+                    return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
+                })
+                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
+                        "Refresh token is not in database!"));
+    }
+
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
---
+++ WorkNet/src/main/java/com/example/worknet/repository/RefreshTokenRepository.java
@@ -0,0 +1,20 @@
+package com.example.worknet.repository;
+
+
+import com.example.worknet.model.RefreshToken;
+import com.example.worknet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    @Override
+    Optional<RefreshToken> findById(Long id);
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
